04840fc950bca9ee797be145ee0facfe1a3bc626
Divyapriya2607 <106463594+Divyapriya2607@users.noreply.github.com>
2024-05-04 10:25:11 +0530

    Added Java Test

---
+++ java_Test/src/demo_package/DemoClass.java
@@ -0,0 +1,25 @@
+package demo_package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import demo_package.DemoClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91975\\Documents\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");
+		driver.findElement(By.id("lst-ib")).sendKeys(Keys.CANCEL);
+		driver.findElement(By.name("btnk")).click();
+		Actions actions=new Actions(driver);
+		
+		
+	}
+}
